970b3b1ab89ae51fbdc2072f7f3c6ef0f4fab1be
Mahin Bin Hasan <80515603+mahinbinhasan@users.noreply.github.com>
2021-12-04 16:34:08 +0600

    Add files via upload

---
+++ ButtonChanger.py
@@ -0,0 +1,28 @@
+from tkinter import *
+root = Tk()
+root.title("Mahin Button Changer")
+root.geometry("500x500")
+
+stop_button = PhotoImage(file='Images/1.png')
+play_button = PhotoImage(file='Images/2.png')
+y = 1
+def play():
+    global y
+    global song
+    if y %2!=0:
+        y = y+1
+        playbtn = Button(Control, image=stop_button, borderwidth=0, command=lambda: play())
+        playbtn.grid(row=0, column=1)
+    else:
+        playbtn = Button(Control, image=play_button, borderwidth=0, command=lambda: play())
+        playbtn.grid(row=0, column=1)
+        y = y+1
+Control = Frame(root)
+Control.pack()
+
+playbtn = Button(Control, image = play_button,borderwidth = 0,command = lambda: play())
+
+playbtn.grid(row = 0,column = 1)
+
+
+root.mainloop()
---
+++ README.txt
@@ -0,0 +1,4 @@
+This program is created by Mahin Bin Hasan
+
+Contacts: 
+Facebook : https://www.facebook.com/Al.mahin.24
