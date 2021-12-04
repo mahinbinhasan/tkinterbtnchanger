30fabc83724dddca1d07a837c9829e7ef10bbe31
Mahin Bin Hasan <80515603+mahinbinhasan@users.noreply.github.com>
2021-12-04 16:41:05 +0600

    Update ButtonChanger.py

--- ButtonChanger.py
+++ ButtonChanger.py
@@ -8,7 +8,6 @@ play_button = PhotoImage(file='Images/2.png')
 y = 1
 def play():
     global y
-    global song
     if y %2!=0:
         y = y+1
         playbtn = Button(Control, image=stop_button, borderwidth=0, command=lambda: play())
